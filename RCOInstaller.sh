3bfcc68b772bc77a560f3cb0a405dc1d62ecf972
VoidSequence <100837615+VoidSequencx@users.noreply.github.com>
2023-08-07 12:55:32 +0900

    Update RCOInstaller.sh

--- RCOInstaller.sh
+++ RCOInstaller.sh
@@ -1,8 +1,8 @@
-cd /Users/user/Applications/Roblox.app/Contents/MacOS
+cd /Users/a/Applications/Roblox.app/Contents/MacOS
 if [ ! -f RCOPATCHED ]; then
 mv RobloxPlayer ActualRobloxPlayer
 echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Users/user/Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Users/user/Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Users/user/Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Users/user/Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
+echo "rm -rf /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Users/a/Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
 touch RCOPATCHED
 echo "Finished installing RCO"
