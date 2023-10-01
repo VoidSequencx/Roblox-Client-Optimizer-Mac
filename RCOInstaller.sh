cc6774dccf278ebfeb50d378a43f08dbb171b9ac
VoidSequence <100837615+VoidSequencx@users.noreply.github.com>
2023-10-02 00:41:26 +0900

    Update RCOInstaller.sh

--- RCOInstaller.sh
+++ RCOInstaller.sh
@@ -1,11 +1,22 @@
-cd /Users/a/Applications/Roblox.app/Contents/MacOS
-if [ ! -f RCOPATCHED ]; then
-mv RobloxPlayer ActualRobloxPlayer
-echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Users/a/Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Users/a/Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
-chmod +x RobloxPlayer
-touch RCOPATCHED
-echo "Finished installing RCO"
-else
-echo "You have already installed RCO!"
+#!/bin/sh
+
+targetFps=200
+robloxPath="/Users/a/Applications/Roblox.app"
+
+if [ ! -d $robloxPath ]; then
+  $robloxPath="~$robloxPath"
+
+  if [ ! -d $robloxPath ]; then
+    echo "Roblox installation folder couldn't be found."
+    exit
+  fi
 fi
+
+clientSettingsPath="$robloxPath/Contents/MacOS/ClientSettings"
+
+if [ ! -d "$clientSettingsPath" ]; then
+  mkdir $clientSettingsPath
+fi
+
+echo "{\"DFIntTaskSchedulerTargetFps\": $targetFps}" > "$clientSettingsPath/ClientAppSettings.json"
+echo "The FPS unlocker has been installed in $robloxPath"
